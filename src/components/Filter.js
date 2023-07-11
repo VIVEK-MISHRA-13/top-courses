25316b058e3fd55ef97e240082dee6f6103c0a12
Vivek Mishra <vm1343396@gmail.com>
2023-07-11 12:30:38 +0530

    first commit

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -8,7 +8,8 @@ const Filter = (props) => {
         setCategory(title);
     }
     return (
-      <div className='w-11/12 flex flex-row max-w-max space-x-4 gap-y-4 mx-auto py-4 justify-center '>
+     <div className='w-11/12  lg:h-fit '>
+         <div className='w-11/12 flex flex-wrap  md:flex-row max-w-max space-x-4 gap-y-4 mx-auto py-4 justify-center '>
           {
               filterData.map((data) => (
                   <button className={`text-lg px-2 py-1 rounded-md font-medium text-white bg-black hover:bg-opacity-50 border-2  transition-all duration-300
@@ -17,6 +18,7 @@ const Filter = (props) => {
               ))
           }
       </div>
+     </div>
     )
 }
 
